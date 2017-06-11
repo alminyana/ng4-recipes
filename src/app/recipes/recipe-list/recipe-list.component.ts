b3781dcc601dd0cb66afa4c65787864b5d3f4c5b
enric <enric.alminyana@i2cat.net>
2017-06-11 10:38:52 +0200

    added recipes to recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply test', 'http://images.media-allrecipes.com/userphotos/560x315/662790.jpg'),
+    new Recipe('A Second Recipe', 'This is a Second Recipe test', 'http://images.media-allrecipes.com/userphotos/250x250/00/69/35/693521.jpg')
+  ];
 
   constructor() { }
 
