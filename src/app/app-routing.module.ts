8d4b6282c4ac98c5e0d03f940ceb340e0fdbfdf8
enric <enric.alminyana@i2cat.net>
2017-08-19 21:08:38 +0200

    new route for recipe-edit and added routerLink in recipe-detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,10 +12,11 @@ const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent },
-    { path: ':id', component: RecipeDetailComponent }
+    { path: ':id', component: RecipeDetailComponent },
+    { path: 'new', component: RecipeEditComponent },
+    { path: ':id/edit', component: RecipeEditComponent }
   ]},
-  { path: 'shopping-list', component:  ShoppingListComponent},
-  { path: 'recipe-edit', component: RecipeEditComponent }
+  { path: 'shopping-list', component:  ShoppingListComponent}
 ];
 
 @NgModule({
